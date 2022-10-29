38711e574ef27e3ad408409aa0c8aff88aa1c748
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-10-29 17:00:49 +0200

    Create update.sh

---
+++ docs/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# this script adds the unix timestap to the library (enforces browser to load the current revision of weblisp.min.js)
+sed -i.bak "s/<script src=\"weblisp.min.js\".*/<script src=\"weblisp.min.js?v=$(date +%s)\"><\/script>/g" index.html
+rm index.html.bak
