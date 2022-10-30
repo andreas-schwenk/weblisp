44b94f416d3a405d363126250a24477de50c2b0f
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-10-30 09:00:19 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,11 +1,15 @@
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
+const { exec } = require("child_process");
 
 esbuild.buildSync({
-    platform: 'browser',
-    globalName: 'weblisp',
-    minify: true,
-    target: 'es2020',
-    entryPoints: ['src/weblisp.js'],
-    bundle: true,
-    outfile: 'build/weblisp.min.js'
+  platform: "browser",
+  globalName: "weblisp",
+  minify: true,
+  target: "es2020",
+  entryPoints: ["src/weblisp.js"],
+  bundle: true,
+  outfile: "build/weblisp.min.js",
 });
+
+exec("cp build/weblisp.min.js docs/");
+exec("cd docs && ./update.sh");
