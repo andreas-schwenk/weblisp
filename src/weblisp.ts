d3d9b91a740fb96d6f54765b69a091f5e771de9d
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-05 15:32:28 +0100

    Update weblisp.ts

--- src/weblisp.ts
+++ src/weblisp.ts
@@ -9,7 +9,8 @@ LISP tutorials:
 
 import { Lexer } from "./lex";
 import { Parser } from "./parse";
-import { SExpr, SExprType } from "./sexpr";
+import { SExpr } from "./sexpr";
+import { Ratio, SExprType as T } from "./types";
 
 export class WebLISP {
   private output = "";
@@ -45,20 +46,20 @@ export class WebLISP {
       throw new RunError("max allowed runtime exceeded!");
     }
     // evaluate
-    let res, u, v: number;
-    let s, t, car, cdr: SExpr;
+    let u, v: number;
+    let s, t, car, cdr, res: SExpr;
     let op = "";
     let i = 0;
     let b = true;
 
     switch (sexpr.type) {
-      case SExprType.NIL:
-      case SExprType.INT:
+      case T.NIL:
+      case T.INT:
         return sexpr;
 
-      case SExprType.CONS:
+      case T.CONS:
         switch (sexpr.car.type) {
-          case SExprType.ID:
+          case T.ID:
             op = sexpr.car.data as string;
             switch (op) {
               // -- one argument --
@@ -68,10 +69,9 @@ export class WebLISP {
               case "CDR":
               case "LENGTH":
               case "SIN":
-                if (
-                  sexpr.cdr.type === SExprType.NIL ||
-                  sexpr.cdr.cdr.type !== SExprType.NIL
-                ) {
+              case "COS":
+              case "TAN":
+                if (sexpr.cdr.type === T.NIL || sexpr.cdr.cdr.type !== T.NIL) {
                   throw new RunError("expected one argument");
                 }
                 switch (op) {
@@ -85,7 +85,7 @@ export class WebLISP {
                   case "CDR":
                   case "LENGTH":
                     s = this.eval(sexpr.cdr.car);
-                    if (s.type !== SExprType.CONS)
+                    if (s.type !== T.CONS)
                       throw new RunError("CAR expects a list");
                     switch (op) {
                       case "CAR":
@@ -94,23 +94,36 @@ export class WebLISP {
                         return s.cdr;
                       case "LENGTH":
                         i = 0;
-                        while (s.type == SExprType.CONS) {
+                        while (s.type == T.CONS) {
                           i++;
                           s = s.cdr;
                         }
                         return SExpr.atomINT(i);
                     }
                   case "SIN":
+                  case "COS":
+                  case "TAN":
                     s = this.eval(sexpr.cdr.car);
-                    if (s.type !== SExprType.FLOAT) xx;
-                    return SExpr.atomFLOAT(Math.sin(s));
+                    if (s.type === T.INT || s.type === T.FLOAT)
+                      v = s.data as number;
+                    else if (s.type === T.RATIO)
+                      v = (s.data as Ratio).toFloat();
+                    else new RunError(op + " expects a number");
+                    switch (op) {
+                      case "SIN":
+                        return SExpr.atomFLOAT(Math.sin(v));
+                      case "COS":
+                        return SExpr.atomFLOAT(Math.cos(v));
+                      case "TAN":
+                        return SExpr.atomFLOAT(Math.tan(v));
+                    }
                 }
               // -- two arguments --
               case "CONS":
                 if (
-                  sexpr.cdr.type === SExprType.NIL ||
-                  sexpr.cdr.cdr.type === SExprType.NIL ||
-                  sexpr.cdr.cdr.cdr.type !== SExprType.NIL
+                  sexpr.cdr.type === T.NIL ||
+                  sexpr.cdr.cdr.type === T.NIL ||
+                  sexpr.cdr.cdr.cdr.type !== T.NIL
                 ) {
                   throw new RunError("expected two arguments");
                 }
@@ -119,25 +132,131 @@ export class WebLISP {
                 return SExpr.cons(car, cdr);
               // -- n arguments (n >= 0) --
               case "+":
-              case "-":
               case "*":
-                res = op === "*" ? 1 : 0;
+                res = SExpr.atomINT(op === "+" ? 0 : 1);
                 (s = sexpr.cdr), (i = 0);
-                while (s.type == SExprType.CONS) {
+                while (s.type === T.CONS) {
                   t = this.eval(s.car);
-                  switch (t.type) {
-                    case SExprType.INT:
-                      if (op === "*") res *= t.data as number;
-                      else res += t.data as number;
-                      break;
-                    default:
-                      throw new RunError(t.toString() + " is not a number");
-                  }
-                  if (i == 0 && op === "-") res = -res;
+                  if (res.type === T.INT && t.type === T.INT) {
+                    if (op === "+") (res.data as number) += t.data as number;
+                    else (res.data as number) *= t.data as number;
+                  } else if (res.type === T.INT && t.type === T.RATIO) {
+                    let u: Ratio;
+                    u = Ratio.fromNumber(res.data as number);
+                    let v = t.data as Ratio;
+                    res.data = op === "+" ? Ratio.add(u, v) : Ratio.mul(u, v);
+                    res.type = T.RATIO;
+                  } else if (res.type === T.RATIO && t.type === T.INT) {
+                    let u = res.data as Ratio;
+                    let v = Ratio.fromNumber(t.data as number);
+                    res.data = op === "+" ? Ratio.add(u, v) : Ratio.mul(u, v);
+                    res.type = T.RATIO;
+                  } else if (res.type === T.RATIO && t.type === T.RATIO) {
+                    let u = res.data as Ratio;
+                    let v = t.data as Ratio;
+                    res.data = op === "+" ? Ratio.add(u, v) : Ratio.mul(u, v);
+                    res.type = T.RATIO;
+                  } else if (
+                    [T.INT, T.RATIO, T.FLOAT].includes(res.type) &&
+                    [T.INT, T.RATIO, T.FLOAT].includes(t.type)
+                  ) {
+                    let u =
+                      res.type === T.RATIO
+                        ? (res.data as Ratio).toFloat()
+                        : (t.data as number);
+                    let v =
+                      t.type === T.RATIO
+                        ? (t.data as Ratio).toFloat()
+                        : (t.data as number);
+                    res.data = op === "+" ? u + v : u * v;
+                    res.type = T.FLOAT;
+                  } else
+                    throw new RunError(
+                      "incompatible types " +
+                        res.type +
+                        " and " +
+                        t.type +
+                        " for op " +
+                        op
+                    );
                   s = s.cdr;
                   i++;
                 }
-                return SExpr.atomINT(res);
+                if (
+                  res.type === T.RATIO &&
+                  (res.data as Ratio).denominator == 1
+                ) {
+                  res.data = (res.data as Ratio).numerator;
+                  res.type = T.INT;
+                }
+                return res;
+              case "-":
+              case "/":
+                res = SExpr.atomINT(op === "-" ? 0 : 1);
+                (s = sexpr.cdr), (i = 0);
+                while (s.type === T.CONS) {
+                  t = this.eval(s.car);
+                  if (i == 0 && s.cdr.type !== T.NIL) {
+                    res.type = t.type;
+                    res.data = t.data;
+                  } else if (res.type === T.INT && t.type === T.INT) {
+                    if (op === "-") (res.data as number) -= t.data as number;
+                    else {
+                      let u = res.data as number;
+                      let v = t.data as number;
+                      res.data = new Ratio(u, v);
+                      res.type = T.RATIO;
+                    }
+                  } else if (res.type === T.INT && t.type === T.RATIO) {
+                    let u: Ratio;
+                    u = Ratio.fromNumber(res.data as number);
+                    let v = t.data as Ratio;
+                    res.data = op === "-" ? Ratio.sub(u, v) : Ratio.div(u, v);
+                    res.type = T.RATIO;
+                  } else if (res.type === T.RATIO && t.type === T.INT) {
+                    let u = res.data as Ratio;
+                    let v = Ratio.fromNumber(t.data as number);
+                    res.data = op === "-" ? Ratio.sub(u, v) : Ratio.div(u, v);
+                    res.type = T.RATIO;
+                  } else if (res.type === T.RATIO && t.type === T.RATIO) {
+                    let u = res.data as Ratio;
+                    let v = t.data as Ratio;
+                    res.data = op === "-" ? Ratio.sub(u, v) : Ratio.div(u, v);
+                    res.type = T.RATIO;
+                  } else if (
+                    [T.INT, T.RATIO, T.FLOAT].includes(res.type) &&
+                    [T.INT, T.RATIO, T.FLOAT].includes(t.type)
+                  ) {
+                    let u =
+                      res.type === T.RATIO
+                        ? (res.data as Ratio).toFloat()
+                        : (t.data as number);
+                    let v =
+                      t.type === T.RATIO
+                        ? (t.data as Ratio).toFloat()
+                        : (t.data as number);
+                    res.data = op === "-" ? u - v : u / v;
+                    res.type = T.FLOAT;
+                  } else
+                    throw new RunError(
+                      "incompatible types " +
+                        res.type +
+                        " and " +
+                        t.type +
+                        " for op " +
+                        op
+                    );
+                  s = s.cdr;
+                  i++;
+                }
+                if (
+                  res.type === T.RATIO &&
+                  (res.data as Ratio).denominator == 1
+                ) {
+                  res.data = (res.data as Ratio).numerator;
+                  res.type = T.INT;
+                }
+                return res;
               // -- n arguments (n >= 1) --
               case ">":
               case ">=":
@@ -146,10 +265,10 @@ export class WebLISP {
                 b = true;
                 u = op === ">" || op === ">=" ? Infinity : -Infinity;
                 (s = sexpr.cdr), (i = 0);
-                while (s.type == SExprType.CONS) {
+                while (s.type == T.CONS) {
                   t = this.eval(s.car);
                   switch (t.type) {
-                    case SExprType.INT:
+                    case T.INT:
                       v = t.data as number;
                       if (
                         (op === ">" && v >= u) ||
@@ -198,7 +317,11 @@ const w = new WebLISP();
 //w.import("(write (+ 3 4 (* 5 6) 7 8 (- 20 10 5) (car (quote (47 11)) ) ) )");
 //w.import("(write (length (quote (3 4 5 6 7 8))))");
 //w.import("(write (cons 3 4))");
-w.import("(write (<= 3 3 5 3 3 3))");
+//w.import("(write (<= 3 3 5 3 3 3))");
+//w.import("(write (sin 1))");
+//w.import("(write (/ 4 6 2 3 4))");
+
+// TODO: (/ (/ 3.0))
 
 try {
   w.run();
