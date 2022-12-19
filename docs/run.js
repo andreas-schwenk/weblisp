83119ebf8643de05f13ccd3d168dde7704390e53
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-19 15:21:37 +0100

    Update run.js

--- docs/run.js
+++ docs/run.js
@@ -1,18 +1,18 @@
-importScripts('weblisp.min.js');
+importScripts("weblisp.min.js");
 
-const w = new weblisp.WebLISP();
+const w = new weblisp.WebLISP(); // TODO: is this line needed??
 
 onmessage = (e) => {
-    let response = '';
-    const w = new weblisp.WebLISP();
-    //console.log(e.data);
-    const src = e.data.src;
-    try {
-        w.import(src);
-        w.run(e.data.maxRuntime);
-        response = w.output.replace(/\n/g, '<br/>');;
-    } catch(e) {
-        response = 'error:' + e; 
-    }
-    postMessage(response);
-}
+  let response = "";
+  const w = new weblisp.WebLISP();
+  //console.log(e.data);
+  const src = e.data.src;
+  try {
+    w.import(src);
+    w.run(e.data.maxRuntime);
+    response = w.output.replace(/\n/g, "<br/>");
+  } catch (e) {
+    response = "error:" + e;
+  }
+  postMessage(response);
+};
