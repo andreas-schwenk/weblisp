cf9eb435cbdef55f3afb0d307c1c6bbf9f87d9ef
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-20 17:03:07 +0100

    Update run.js

--- docs/run.js
+++ docs/run.js
@@ -1,16 +1,27 @@
 importScripts("weblisp.min.js");
 
-const w = new weblisp.WebLISP(); // TODO: is this line needed??
+//const w = new weblisp.WebLISP(); // TODO: is this line needed??
 
 onmessage = (e) => {
   let response = "";
   const w = new weblisp.WebLISP();
   //console.log(e.data);
   const src = e.data.src;
+  const breakpoints = e.data.breakpoints;
   try {
     w.import(src);
+    for (const b of breakpoints) {
+      w.addBreakpoint(b + 1);
+    }
     w.run(e.data.maxRuntime);
     response = w.output.replace(/\n/g, "<br/>");
+    const d = w.getDebugInfo();
+    for (const di of d) {
+      response += "<br/>== BREAKPOINT " + di.breakpointLine + " ==<br/>";
+      for (const vid in di.variableValues) {
+        response += vid + ": " + di.variableValues[vid];
+      }
+    }
   } catch (e) {
     response = "error:" + e;
   }
