c45e560f0372d191e0026e25e3eedacee212fdde
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-02 11:27:22 +0100

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -1,4 +1,5 @@
 import * as esbuild from "esbuild";
+import fs from "fs";
 import { exec } from "child_process";
 
 esbuild.buildSync({
@@ -10,6 +11,21 @@ esbuild.buildSync({
   bundle: true,
   outfile: "build/weblisp.min.js",
 });
+
+esbuild.buildSync({
+  platform: "browser",
+  globalName: "RUNTIME",
+  minify: true,
+  target: "es2020",
+  entryPoints: ["src/runtime.ts"],
+  bundle: true,
+  outfile: "build/runtime.min.js",
+});
+
+let runtimeCode = fs.readFileSync("build/runtime.min.js", "utf-8");
+runtimeCode = "export const runtimeCode = `" + runtimeCode + "`;";
+fs.writeFileSync("src/runtimeCode.ts", runtimeCode);
+//console.log(runtimeCode);
 
 exec("cp build/weblisp.min.js docs/");
 exec("cd docs && ./update.sh");
