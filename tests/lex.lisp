b363b4f9bc54a033407634e42723185980bd5693
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-06 17:30:55 +0100

    Create lex.lisp

---
+++ tests/lex.lisp
@@ -0,0 +1,34 @@
+; TOKEN = { WS } ( KW | ID | INT | DEL );
+; KW = "int" | "if" | "for" | "while";
+; ID = ALPHA { ALPHA | NUM };
+; INT = "0" | NUM1 { NUM };
+; DEL = "=" | ";";
+; ALPHA = "a".."z" | "A".."Z" | "_";
+; NUM = "0..9";
+; ALPHA_NUM = ALPHA | NUM;
+; NUM1 = "1..9";
+; WS = "\n" | " " | "\t";
+
+; example input string:
+; "int x = 4;" -> 
+;   (kw "int")
+;   (id "x")
+;   (del "=")
+;   (int "4")
+;   (ter ";")
+
+(setf input "int x = 4;")
+(setf c #\0)   ; TODO: #\zero
+(setf pos -1)
+
+(defun next ()
+    (setf pos (+ pos 1))
+    (setf c (char input pos))
+    c)
+
+(rewrite '(start "" c)
+    (start TK #\i) -> (id-i "i" (next))
+    (id-i TK #\f) -> (kw "if")
+    (id-i TK #\n) -> (id-in "in" (next))
+    (id-in TK #\t) -> (kw "int")
+)
