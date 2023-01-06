831dffcc4b05f39c43e06f92b1cd4895c893ab90
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-06 17:30:47 +0100

    Create 105.lisp

---
+++ tests/105.lisp
@@ -0,0 +1,41 @@
+; TRS: diff with non-binary operators + and *
+(setf d
+    (rewrite '(diff (+ 314 271 1337 (* 3 x)) x)
+    
+        '(diff $x $v)
+            (numberp x)
+            0
+
+        '(diff $x $v)      
+            (equalp x v)
+            1
+
+        '(diff (+ $x $y) $v)
+            T
+            `(+ (diff ,x ,v) (diff ,y ,v))
+
+        '(diff (+ $x $y $$z) $v)
+            T
+            `(+ (diff ,x ,v) (diff ,y ,v) (diff ,(append (+) z) ,v))
+
+        '(diff (* $x $y) $v)
+            T
+            `(+ (* (diff ,x ,v) ,y) (* ,x (diff ,y ,v)))
+        
+        '(+ $x $y)      
+            (and (numberp x) (numberp y))   
+            (+ x y)
+
+        '(+ $x $y $$z)      
+            (and (numberp x) (numberp y))
+            (append `(+ ,(+ x y)) z)
+
+        '(+ $x 0) T x
+        '(+ 0 $x) T x
+        '(* $x 0) T 0
+        '(* 0 $x) T 0
+        '(* $x 1) T x
+        '(* 1 $x) T x
+    ))
+(write d)
+(equalp d 3)
