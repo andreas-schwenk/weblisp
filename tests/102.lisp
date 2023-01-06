998b0ca773ba40450a27b27f676f8dfec15a6261
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-06 17:30:37 +0100

    Update 102.lisp

--- tests/102.lisp
+++ tests/102.lisp
@@ -1,15 +1,7 @@
 ; TRS
-(equalp 7
+(setf v
     (rewrite '(1 2 3)
         '(1 2 3)   T  '(4 5 6)
         '(4 5 $x)  T  x))
-
-;(setf derivate 
-;    (rules
-;        (diff (+ $x $y) $v) -> (+ (diff x v) (diff y v) )
-;        (diff (* $x $y) $v) -> (+ (* (diff x v) y) (* u (diff y v)) )
-;        (diff $v $v) -> 1
-;        (diff $u $v) [(not (equalp u v))] -> 0))
-;(write
-;    (rewrite (diff 1337) derivate))
-; TODO: non-binary "+" and "*"
+(write v)
+(equalp v 6)
