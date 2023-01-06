18d0c0c9240da68aaa78ece19ceb8b852c6d018d
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-06 17:29:51 +0100

    Update parse.ts

--- src/parse.ts
+++ src/parse.ts
@@ -1,20 +1,26 @@
-/* webLISP, 2022 by Andreas Schwenk */
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+// TODO: add comments to this file
 
 import { Lexer } from "./lex";
+import { TRS_Parser } from "./parseTRS";
 import { SExpr } from "./sexpr";
 import { SExprType } from "./types";
 
 export class Parser {
-  public static parse(lexer: Lexer): SExpr[] {
+  public parse(lexer: Lexer): SExpr[] {
     const res: SExpr[] = [];
     while (!lexer.isEof()) {
-      res.push(this.parseRec(lexer));
+      res.push(this.parseSExpr(lexer));
     }
     return res;
   }
 
-  //G sexpr = "#'" sexpr | "'" sexpr | "`" sexpr | "(" { sexpr } ")" | "T" | "NIL" | "." | INT | REAL | ID;
-  private static parseRec(lexer: Lexer): SExpr {
+  //G sexpr = "#'" sexpr | "'" sexpr | "`" sexpr | "(" { sexpr } ")" | "T" | "NIL" | "." | INT | REAL | CHAR | STR | ID;
+  public parseSExpr(lexer: Lexer): SExpr {
     if (lexer.getToken() === "T") {
       const sexpr = SExpr.atomT(lexer.getRow(), lexer.getCol());
       lexer.next();
@@ -45,23 +51,34 @@ export class Parser {
           break;
       }
       lexer.next();
-      const s = this.parseRec(lexer);
-      let sexpr: SExpr = SExpr.cons(
+      const s = this.parseSExpr(lexer);
+      let sexpr = SExpr.cons(
         SExpr.atomID(id),
         SExpr.cons(s, SExpr.atomNIL()),
         lexer.getRow(),
         lexer.getCol()
       );
       return sexpr;
-    } else if (lexer.getToken() === "(") {
+    } else if (lexer.getToken() === "(" || lexer.getToken() === "[") {
+      // TODO: only allow brackets in TRS mode!
+      const brackets = lexer.getToken() === "[";
       lexer.next();
+      let parsingTRS = false;
+      if (lexer.getToken() === "TRS") {
+        parsingTRS = true;
+        lexer.activateTrsMode(true);
+      }
       let sexpr: SExpr = null;
       let first: SExpr = SExpr.atomNIL(lexer.getRow(), lexer.getCol());
       let i = 0;
       let dot = false;
       let dots = 0;
-      while (!lexer.isEof() && lexer.getToken() !== ")") {
-        const s = this.parseRec(lexer);
+      while (
+        !lexer.isEof() &&
+        lexer.getToken() !== ")" &&
+        lexer.getToken() !== "]"
+      ) {
+        const s = this.parseSExpr(lexer);
         if (s.type === SExprType.ID && s.data === ".") {
           if (i == 0)
             throw new ParseError(
@@ -90,16 +107,32 @@ export class Parser {
           lexer.getRow(),
           lexer.getCol()
         );
-      if (lexer.getToken() === ")") lexer.next();
-      else throw new ParseError("expected ')'", lexer.getRow(), lexer.getCol());
-      return first;
+      if (
+        (!brackets && lexer.getToken() === ")") ||
+        (brackets && lexer.getToken() === "]")
+      )
+        lexer.next();
+      else
+        throw new ParseError(
+          "expected '" + (brackets ? "]" : ")") + "'",
+          lexer.getRow(),
+          lexer.getCol()
+        );
+      if (parsingTRS) {
+        lexer.activateTrsMode(false);
+        const trsParser = new TRS_Parser(this);
+        first = trsParser.TRS_postprocess(first);
+      }
+      return brackets ? this.embedIntoId("COMMA", first) : first;
     } else if (lexer.getToken() === ".") {
       const sexpr = SExpr.atomID(".", lexer.getRow(), lexer.getCol());
       lexer.next();
       return sexpr;
     } else if (
       (lexer.getToken()[0] >= "0" && lexer.getToken()[0] <= "9") ||
-      (lexer.getToken().length > 1 && lexer.getToken()[0] == "-")
+      (lexer.getToken().length > 1 &&
+        lexer.getToken()[0] == "-" &&
+        lexer.getToken()[1] != ">")
     ) {
       const tk = lexer.getToken();
       let sexpr: SExpr;
@@ -115,12 +148,38 @@ export class Parser {
       else sexpr = SExpr.atomINT(parseInt(tk), lexer.getRow(), lexer.getCol());
       lexer.next();
       return sexpr;
+    } else if (lexer.getToken().startsWith("#\\")) {
+      // TODO: check, if valid
+      const tk = lexer.getToken();
+      const sexpr = SExpr.atomCHAR(tk.substring(2));
+      lexer.next();
+      return sexpr;
+    } else if (lexer.getToken()[0] == '"') {
+      const tk = lexer.getToken();
+      const sexpr = SExpr.atomSTRING(tk.substring(1, tk.length - 1));
+      lexer.next();
+      return sexpr;
     } else {
-      const value = lexer.getToken();
+      let value = lexer.getToken();
       const sexpr = SExpr.atomID(value, lexer.getRow(), lexer.getCol());
       lexer.next();
       return sexpr;
     }
+  }
+
+  public embedIntoId(id: string, sexpr: SExpr): SExpr {
+    return SExpr.cons(
+      SExpr.atomID(id),
+      SExpr.cons(sexpr, SExpr.atomNIL()),
+      sexpr.srcRow,
+      sexpr.srcCol
+    );
+  }
+
+  public generateCall(id: string, args: SExpr[]): SExpr {
+    let e: SExpr = SExpr.atomNIL();
+    for (let k = args.length - 1; k >= 0; k--) e = SExpr.cons(args[k], e);
+    return SExpr.cons(SExpr.atomID(id), e);
   }
 }
 
