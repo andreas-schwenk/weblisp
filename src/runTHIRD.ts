c4eade58ef224081d7749382030a096b703898c3
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:42:41 +0100

    Create runTHIRD.ts

---
+++ src/runTHIRD.ts
@@ -0,0 +1,15 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runTHIRD(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  if (this.check) this.checkArgCount(sexpr, 1);
+  let param = this.eval(sexpr.cdr.car);
+  return SExpr.nth(param, 2);
+}
