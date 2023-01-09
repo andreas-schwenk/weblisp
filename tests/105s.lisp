19385da6104c1cc486b51859c72151860ace1086
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 09:57:16 +0100

    Update 105s.lisp

--- tests/105s.lisp
+++ tests/105s.lisp
@@ -1,30 +1,5 @@
-; same as 105.lisp, with syntactic sugar:
-; TRS-environment:
-;   (trs u {s "->" t})
-; Uppercase identifiers are considered as variables.
-; Type definitions in form
-;   ":" type
-; may follow.
+(setf x
+    (trs '(1 2 3 4 5)
+        (1 2 X*) -> (a ~ X b ~ X)))
 
-; TODO: conditions
-
-; TODO: "append" is ugly!
-
-(setf d
-    (TRS '(diff (+ 314 271 1337 (* 3 x)) x)
-        (diff X:number V) -> 0
-        (diff V V) -> 1
-        (diff (+ X Y) V) -> (+ (diff X V) (diff Y V))
-        (diff (+ X Y Z*) V) -> (+ (diff X V) (diff Y V) (diff [append '(+) Z] V))
-        (diff (* X Y) V) -> (+ (* (diff X V) Y) (* X (diff Y V)))
-        (+ X:number Y:number) -> [+ X Y]
-        (+ X:number Y:number Z*) -> [append `(+ [+ X Y]) Z]
-        (+ X 0) -> X
-        (+ 0 X) -> X
-        (* X 0) -> 0
-        (* 0 X) -> 0
-        (* X 1) -> X
-        (* 1 X) -> X
-))
-(write d)
-(equalp d 3)
+(equalp x '(a 3 4 5 b 3 4 5))
