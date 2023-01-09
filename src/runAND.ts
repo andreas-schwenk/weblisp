ce47ec0895df04c20b87b216c6e893b9736c4ccd
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:40:42 +0100

    Create runAND.ts

---
+++ src/runAND.ts
@@ -0,0 +1,19 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runAND(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  let res = SExpr.atomT();
+  for (let s = sexpr.cdr; s.type === T.CONS; s = s.cdr) {
+    const t = this.eval(s.car);
+    if (t.type === T.NIL) return t;
+    res = t;
+  }
+  return res;
+}
