886b31cb5719d09f18edf9f3d591184c0b938ee3
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:41:45 +0100

    Create runLENGTH.ts

---
+++ src/runLENGTH.ts
@@ -0,0 +1,29 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runLENGTH(this: WebLISP, sexpr: SExpr): SExpr {
+  if (this.interpret) {
+    if (this.check) this.checkArgCount(sexpr, 1);
+    let param = this.eval(sexpr.cdr.car);
+    let len;
+    for (len = 0; param.type === T.CONS; len++) param = param.cdr;
+    return SExpr.atomINT(len);
+  } else {
+    const list = this.genVar();
+    const ctr = this.genVar();
+    const param = this.eval(sexpr.cdr.car);
+    const c =
+      `let ${list}=${param};\n` +
+      `let ${ctr};\n` +
+      `for(${ctr}=0; ${list}.type === SExprType.CONS; ${ctr}++)\n` +
+      `  ${list} = ${list}.cdr;\n`;
+    this.code += c;
+    return SExpr.atomSTRING(`SExpr.atomINT(${ctr})`);
+  }
+}
