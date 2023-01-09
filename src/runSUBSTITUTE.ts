7aa60988df81230fa812242c7b5379888134fc74
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:42:34 +0100

    Create runSUBSTITUTE.ts

---
+++ src/runSUBSTITUTE.ts
@@ -0,0 +1,19 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runSUBSTITUTE(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  // TODO: SUBSTITUTE AND SUBST ARE NOT EQUAL!!
+  if (this.check) this.checkArgCount(sexpr, 3);
+  const newExpr = this.eval(SExpr.nth(sexpr, 1));
+  const oldExpr = this.eval(SExpr.nth(sexpr, 2));
+  const tree = this.eval(SExpr.nth(sexpr, 3));
+  const res = SExpr.subst(newExpr, oldExpr, tree);
+  return res;
+}
