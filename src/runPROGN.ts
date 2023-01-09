28d2f5650c3eea3e8593c4f664557cc8a012b5d6
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:42:17 +0100

    Create runPROGN.ts

---
+++ src/runPROGN.ts
@@ -0,0 +1,15 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runPROGN(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  let res = SExpr.atomNIL();
+  for (let s = sexpr.cdr; s.type === T.CONS; s = s.cdr) res = this.eval(s.car);
+  return res;
+}
