e65ade4c9aa79152fcc9345b3d433ac4aeec43a0
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:41:25 +0100

    Create runEQUALP.ts

---
+++ src/runEQUALP.ts
@@ -0,0 +1,16 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runEQUALP(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  if (this.check) this.checkArgCount(sexpr, 2);
+  return SExpr.equalp(this.eval(sexpr.cdr.car), this.eval(sexpr.cdr.cdr.car))
+    ? SExpr.atomT()
+    : SExpr.atomNIL();
+}
