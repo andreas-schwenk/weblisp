46cd0c0ac0444cbe17adff87327453b96c033bf7
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:40:49 +0100

    Create runAPPLY.ts

---
+++ src/runAPPLY.ts
@@ -0,0 +1,19 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runAPPLY(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  // (APPLY function parameterList)
+  if (this.check) this.checkMinArgCount(sexpr, 2);
+  const fun = this.eval(sexpr.cdr.car);
+  if (this.check && fun.type !== T.DEFUN)
+    throw new RunError("expected a function");
+  const args = this.eval(sexpr.cdr.cdr.car);
+  return this.call(fun.cdr.car, args, fun.cdr.cdr);
+}
