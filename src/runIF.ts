b95bce7902cec812f8d99da84a24f538b175c6cf
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:41:40 +0100

    Create runIF.ts

---
+++ src/runIF.ts
@@ -0,0 +1,17 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runIF(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  // (IF cond codeT codeF)
+  this.checkMinArgCount(sexpr, 2);
+  if (this.eval(SExpr.nth(sexpr, 1)).type !== T.NIL)
+    return this.eval(SExpr.nth(sexpr, 2));
+  else return this.eval(SExpr.nth(sexpr, 3));
+}
