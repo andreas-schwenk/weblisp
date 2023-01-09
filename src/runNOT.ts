94f8a1f3d70ae43b05b64eb7ede7b717f670429c
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:41:56 +0100

    Create runNOT.ts

---
+++ src/runNOT.ts
@@ -0,0 +1,16 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runNOT(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  // same as NULL
+  if (this.check) this.checkArgCount(sexpr, 1);
+  let param = this.eval(sexpr.cdr.car);
+  return param.type === T.NIL ? SExpr.atomT() : SExpr.atomNIL();
+}
