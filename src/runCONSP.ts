cd43d057a3d1279f4f6dc290a157140af7975995
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:41:08 +0100

    Create runCONSP.ts

---
+++ src/runCONSP.ts
@@ -0,0 +1,15 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runCONSP(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  if (this.check) this.checkArgCount(sexpr, 1);
+  let param = this.eval(sexpr.cdr.car);
+  return param.type === T.CONS ? SExpr.atomT() : SExpr.atomNIL();
+}
