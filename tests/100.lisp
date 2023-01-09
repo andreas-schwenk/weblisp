2b2bb158d4b721641445edc9e75f475fe24a0c86
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 17:44:58 +0100

    Update 100.lisp

--- tests/100.lisp
+++ tests/100.lisp
@@ -1,5 +1,12 @@
 ; TRS
-(equalp 'z
-    (rewrite 'x
-        'x T 'y
-        'y T 'z))
+;; (equalp 'z
+;;     (rewrite 'x
+;;         'x T 'y
+;;         'y T 'z))
+
+(setf my-trs '(
+    'x T 'y
+    'y T 'z))
+(write my-trs)
+(equalp 'z 
+    (rewrite 'x my-trs))
