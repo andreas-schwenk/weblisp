d8d20a79e8740736766ad550b5898eecffcaee67
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:41:30 +0100

    Create runFUNCTION.ts

---
+++ src/runFUNCTION.ts
@@ -0,0 +1,19 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runFUNCTION(this: WebLISP, sexpr: SExpr): SExpr {
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  if (this.check) this.checkArgCount(sexpr, 1);
+  const idExpr = sexpr.cdr.car;
+  if (this.check && idExpr.type !== T.ID) throw new RunError("expected ID");
+  const id = idExpr.data as string;
+  if (this.check && id in this.functions == false)
+    throw new RunError("undefined function " + (id as string));
+  return SExpr.defun(id, this.functions[id as string]);
+}
