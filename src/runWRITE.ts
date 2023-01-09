cf802c7ad12562037d5e27515ea0a46ca3915b4b
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:42:51 +0100

    Create runWRITE.ts

---
+++ src/runWRITE.ts
@@ -0,0 +1,24 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runWRITE(this: WebLISP, sexpr: SExpr): SExpr {
+  if (this.interpret) {
+    if (this.check) this.checkArgCount(sexpr, 1);
+    const param = this.eval(sexpr.cdr.car);
+    this.output += param.toString();
+    console.log(param.toString());
+    return param;
+  } else {
+    const v = this.genVar();
+    const c = `let ${v} = ${this.eval(sexpr.cdr.car)};`;
+    this.code += c;
+    this.code += `console.log(${v}.toString());`;
+    return SExpr.atomSTRING(v);
+  }
+}
