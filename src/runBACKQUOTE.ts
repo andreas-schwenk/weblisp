93328404dd63e6de63edfa083ed3cdc6ec2f0469
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-09 14:40:55 +0100

    Create runBACKQUOTE.ts

---
+++ src/runBACKQUOTE.ts
@@ -0,0 +1,25 @@
+/* 
+  webLISP, 2022-2023 by Andreas Schwenk <contact@compiler-construction.com>
+  LICENSE: GPLv3 
+*/
+
+import { SExpr } from "./sexpr";
+import { SExprType as T } from "./types";
+import { RunError, WebLISP } from "./weblisp";
+
+export function runBACKQUOTE(this: WebLISP, sexpr: SExpr): SExpr {
+  // TODO: move code!
+  if (!this.interpret) throw new RunError("UNIMPLEMENTED");
+  if (this.check) this.checkArgCount(sexpr, 1);
+  return this.runBACKQUOTE_core(sexpr.cdr.car);
+}
+
+export function runBACKQUOTE_core(this: WebLISP, s: SExpr): SExpr {
+  if (s.type === T.CONS) {
+    if (s.car.type === T.ID && (s.car.data as string) === "COMMA")
+      return this.eval(s.cdr.car);
+    s.car = this.runBACKQUOTE_core(s.car);
+    s.cdr = this.runBACKQUOTE_core(s.cdr);
+  }
+  return s;
+}
