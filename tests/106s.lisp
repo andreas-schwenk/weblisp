d884cc34ab4f329be785a7ce014082bf45665334
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-10 10:18:44 +0100

    Update 106s.lisp

--- tests/106s.lisp
+++ tests/106s.lisp
@@ -1,6 +1,6 @@
-; same as 105.lisp, with syntactic sugar:
+; same as 106.lisp, with syntactic sugar:
 ; TRS-environment:
-;   (trs u {s "->" t})
+;   (trs {s "->" t})
 ; Uppercase identifiers are considered as variables.
 ; Type definitions in form
 ;   ":" type
@@ -8,14 +8,13 @@
 
 ; TODO: conditions
 
-; TODO: "append" is ugly!
-
 (setf diff-rules (trs
     (diff X:number V) -> 0
     (diff V V) -> 1
     (diff (+ X Y) V) -> (+ (diff X V) (diff Y V))
     (diff (+ X Y Z*) V) -> (+ (diff X V) (diff Y V) (diff (+ ~ Z) V))
     (diff (* X Y) V) -> (+ (* (diff X V) Y) (* X (diff Y V)))
+    (diff (sin X) V) -> (* (diff X V) (cos X))
     (+ X:number Y:number) -> [+ X Y]
     (+ X:number Y:number Z*) -> (+ [+ X Y] ~ Z)
     (+ X 0) -> X
@@ -43,5 +42,8 @@
             x) 
         diff-rules)) 
     "test 2 failed")
+
+(setf d (rewrite '(diff (sin (* 2 x)) x) diff-rules))
+(assert (equalp d '(* 2 (COS (* 2 X)))) "test 3 failed")
 
 T
