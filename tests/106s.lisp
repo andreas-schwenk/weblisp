fc08ac92e47b989c886324250779471cb20923df
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-01-10 09:56:03 +0100

    Update 106s.lisp

--- tests/106s.lisp
+++ tests/106s.lisp
@@ -10,7 +10,7 @@
 
 ; TODO: "append" is ugly!
 
-(setf derivate (trs
+(setf diff-rules (trs
     (diff X:number V) -> 0
     (diff V V) -> 1
     (diff (+ X Y) V) -> (+ (diff X V) (diff Y V))
@@ -20,16 +20,28 @@
     (+ X:number Y:number Z*) -> (+ [+ X Y] ~ Z)
     (+ X 0) -> X
     (+ 0 X) -> X
+    (* X:number Y:number) -> [* X Y]
+    (* X:number Y:number Z*) -> (* [* X Y] ~ Z)
     (* X 0) -> 0
     (* 0 X) -> 0
     (* X 1) -> X
     (* 1 X) -> X
 ))
 
-(setf d
-    (rewrite '(diff (+ 314 271 1337 (* 3 x)) x) derivate))
+(assert (equalp 3
+    (rewrite 
+        '(diff 
+            (+ 314 271 1337 (* 3 x)) 
+            x) 
+        diff-rules)) 
+    "test 1 failed")
 
-(write d)
-(equalp d 3)
+(assert (equalp '(+ x x)
+    (rewrite 
+        '(diff 
+            (* x x) 
+            x) 
+        diff-rules)) 
+    "test 2 failed")
 
-; TODO: define asserts to easily check multiple tests per file
+T
